118c0c49ef78261c3aca3398dc25dfa727265882
Franky <frankyan2001@gmail.com>
2025-08-12 22:14:48 -0400

    Creando migracion de tasks_assignees

--- task-api/database/migrations/1755049570892_create_task_assignees_table.ts
+++ task-api/database/migrations/1755049570892_create_task_assignees_table.ts
@@ -2,17 +2,16 @@ import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
   protected tableName = 'task_assignees'
-
-  async up() {
+  
+  async up () {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
+      table.integer('task_id').unsigned().references('tasks.id').onDelete('CASCADE')
+      table.integer('user_id').unsigned().references('users.id')
+      table.timestamp('assigned_at', { useTz: true }).defaultTo(this.now())
+      table.primary(['task_id','user_id'])
     })
   }
-
-  async down() {
-    this.schema.dropTable(this.tableName)
+  async down () { 
+    this.schema.dropTable(this.tableName) 
   }
 }
