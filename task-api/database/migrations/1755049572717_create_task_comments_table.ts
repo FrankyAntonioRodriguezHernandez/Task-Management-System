c3d67f64e9abfe4b1e1a8b8c2a488e9be271b4e1
Franky <frankyan2001@gmail.com>
2025-08-12 22:16:13 -0400

    Creando migracion de tasks_comments

--- task-api/database/migrations/1755049572717_create_task_comments_table.ts
+++ task-api/database/migrations/1755049572717_create_task_comments_table.ts
@@ -2,17 +2,16 @@ import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
   protected tableName = 'task_comments'
-
-  async up() {
+  
+  async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
+      table.integer('task_id').unsigned().references('tasks.id').onDelete('CASCADE')
+      table.integer('user_id').unsigned().references('users.id')
+      table.text('comment').notNullable()
+      table.timestamp('created_at', { useTz: true }).defaultTo(this.now())
+      table.timestamp('updated_at', { useTz: true }).defaultTo(this.now())
     })
   }
-
-  async down() {
-    this.schema.dropTable(this.tableName)
-  }
+  async down () { this.schema.dropTable(this.tableName) }
 }
