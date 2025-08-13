d0673d97aeaee57c97dd58c94f5a1a819ab8adff
Franky <frankyan2001@gmail.com>
2025-08-12 22:06:20 -0400

    Creando migracion de tasks

--- task-api/database/migrations/1755049565388_create_tasks_table.ts
+++ task-api/database/migrations/1755049565388_create_tasks_table.ts
@@ -3,16 +3,20 @@ import { BaseSchema } from '@adonisjs/lucid/schema'
 export default class extends BaseSchema {
   protected tableName = 'tasks'
 
-  async up() {
+  async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
+      table.string('title').notNullable()
+      table.enu('status', ['in_progress','reviews','completed','done'], { useNative: true, enumName: 'task_status' }).notNullable()
+      table.integer('created_by').unsigned().references('users.id').onDelete('SET NULL')
+      table.timestamp('deleted_at', { useTz: true }).nullable() // Soft delete
+      table.timestamp('created_at', { useTz: true }).defaultTo(this.now())
+      table.timestamp('updated_at', { useTz: true }).defaultTo(this.now())
     })
   }
 
-  async down() {
+  async down () {
     this.schema.dropTable(this.tableName)
+    this.schema.raw('DROP TYPE IF EXISTS task_status')
   }
 }
