595e49c3bd6bf4d4fe2b78214fdcf4964a3fba12
Franky <frankyan2001@gmail.com>
2025-08-12 21:59:35 -0400

    Creando migracion de user

--- task-api/database/migrations/1755049545934_create_users_table.ts
+++ task-api/database/migrations/1755049545934_create_users_table.ts
@@ -3,16 +3,18 @@ import { BaseSchema } from '@adonisjs/lucid/schema'
 export default class extends BaseSchema {
   protected tableName = 'users'
 
-  async up() {
+  async up () {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
+      table.increments('id') // SERIAL
+      table.string('email').notNullable().unique()
+      table.string('full_name').notNullable()
+      table.string('avatar_url').nullable()
+      table.timestamp('created_at', { useTz: true }).defaultTo(this.now())
+      table.timestamp('updated_at', { useTz: true }).defaultTo(this.now())
     })
   }
 
-  async down() {
+  async down () {
     this.schema.dropTable(this.tableName)
   }
 }
