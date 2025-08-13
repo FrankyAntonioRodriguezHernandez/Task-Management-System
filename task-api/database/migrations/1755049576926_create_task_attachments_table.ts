091e0de802497d515c6b5eef481f66b5c23cdb48
Franky <frankyan2001@gmail.com>
2025-08-12 22:17:35 -0400

    Creando migracion de tasks_attachments

--- task-api/database/migrations/1755049576926_create_task_attachments_table.ts
+++ task-api/database/migrations/1755049576926_create_task_attachments_table.ts
@@ -2,17 +2,17 @@ import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
   protected tableName = 'task_attachments'
-
-  async up() {
+  
+  async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
+      table.integer('task_id').unsigned().references('tasks.id').onDelete('CASCADE')
+      table.string('file_name').notNullable()
+      table.string('file_path').notNullable()
+      table.integer('file_size').notNullable()
+      table.integer('uploaded_by').unsigned().references('users.id')
+      table.timestamp('created_at', { useTz: true }).defaultTo(this.now())
     })
   }
-
-  async down() {
-    this.schema.dropTable(this.tableName)
-  }
+  async down () { this.schema.dropTable(this.tableName) }
 }
