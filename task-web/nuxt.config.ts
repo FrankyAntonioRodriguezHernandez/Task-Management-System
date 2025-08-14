c1e5581372f262c02eb1a43ca11feff853db422b
Franky <frankyan2001@gmail.com>
2025-08-14 14:09:28 -0400

    Instalando dependencias y componentes ui

--- task-web/nuxt.config.ts
+++ task-web/nuxt.config.ts
@@ -1,18 +1,24 @@
-import { defineNuxtConfig } from 'nuxt/config'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  modules: ['@pinia/nuxt'],
+  modules: ['@pinia/nuxt', 'shadcn-nuxt'],
+  css: ['~/assets/css/tailwind.css'],
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+    ],
   },
-  runtimeConfig: {
-    public: { apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:3333' },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
   },
-  typescript: {
-    typeCheck: true
-  }
 })
--- task-web/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export function useApi() {
-  const config = useRuntimeConfig()
-  return axios.create({
-    baseURL: config.public.apiBase,
-    headers: { 'Content-Type': 'application/json' },
-  })
-}
