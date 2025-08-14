f5d93ad1d073d3664210eb6d8aeaed0ab8eacdfa
Franky <frankyan2001@gmail.com>
2025-08-14 01:49:23 -0400

    Instalando dependencias y componentes ui

--- task-web/nuxt.config.ts
+++ task-web/nuxt.config.ts
@@ -5,4 +5,19 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   vite: { plugins: [tailwindcss()] },
+  modules: ['shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+  typescript: {
+    typeCheck: true
+  },
 })
--- task-web/tailwind.config.ts
+++ task-web/tailwind.config.ts
@@ -1,5 +1,14 @@
 import type { Config } from 'tailwindcss'
 
-export default <Partial<Config>>({
+export default <Partial<Config>>{
   darkMode: 'class',
-})
+  content: [
+    './components/**/*.{vue,ts}',
+    './layouts/**/*.{vue,ts}',
+    './pages/**/*.{vue,ts}',
+    './app/**/*.{vue,ts}',
+    './plugins/**/*.{ts}',
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
